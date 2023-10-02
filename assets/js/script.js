2de5fa961dbab7d5c2cd420fc7fb602e8db38f64
gfuselier <gisellefuselier@gmail.com>
2023-10-02 13:11:00 -0500

    added api keys to .js

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+// OMDB:
+// Data request - http://www.omdbapi.com/?apikey=[yourkey]&
+// Poster request - http://img.omdbapi.com/?apikey=[yourkey]&
+
+var omdbKey = "dcd25d25"
+
+// The Movie Database:
+// Example data request - https://api.themoviedb.org/3/search/movie?query={search}&api_key={key}
+var tmdKey = "91ce5d26720f6e04f0cc120d15c7cd71"
