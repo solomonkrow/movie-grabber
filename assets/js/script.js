9f40b75798ecba56b08ef121bf6b7cfa5f7f35ca
Holden Harvey <datgoolguy1@gmail.com>
2023-10-02 14:10:53 -0500

    message

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,3 +7,21 @@ var omdbKey = "dcd25d25"
 // The Movie Database:
 // Example data request - https://api.themoviedb.org/3/search/movie?query={search}&api_key={key}
 var tmdKey = "91ce5d26720f6e04f0cc120d15c7cd71"
+
+function fetchMovieDetails() {
+    const options = {method: 'GET', headers: {accept: 'application/json'}};
+
+    fetch('https://api.themoviedb.org/3/authentication', options)
+      .then(response => response.json())
+      .then(response => console.log(response))
+      .catch(err => console.error(err));
+}
+console.log(fetchMovieDetails);
+// function fetchSpecificMovie() {
+
+// }
+
+
+// var button = document.addEventListener ("click", fetchSpecificMovie)
+
+fetchMovieDetails()
