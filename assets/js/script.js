0420b0a79f1b4c2c5916782e25261c93e399009f
gfuselier <gisellefuselier@gmail.com>
2023-10-03 10:26:58 -0500

    started fetch function

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,3 +7,14 @@ var omdbKey = "dcd25d25"
 // The Movie Database:
 // Example data request - https://api.themoviedb.org/3/search/movie?query={search}&api_key={key}
 var tmdKey = "91ce5d26720f6e04f0cc120d15c7cd71"
+
+searchBtn.addEventListener("click", function(event) {
+    event.preventDefault();
+    var searchInput = searchBox.value.trim();
+
+    findMovie(searchInput)
+})
+
+function findMovie(searchInput) {
+    var omdbUrl = "http://www.omdbapi.com/?apikey=" + omdbKey + "&t=" + searchInput;
+}
