46b18172bd69f957d5f2e2f2ba4e67fdadcf2bcb
gfuselier <gisellefuselier@gmail.com>
2023-10-04 11:48:37 -0500

    finalized card styling

--- assets/js/script.js
+++ assets/js/script.js
@@ -72,22 +72,38 @@ function findMovie(searchInput) {
       console.log(data.Title, data.Plot, data.Genre, data.Poster)   
         //need to make this a loop
       var card = document.createElement("div")
-      var title = document.createElement("h3")
+      card.setAttribute("class", "column is-full")
+      var div2 = document.createElement("div")
+      div2.setAttribute("class", "card is-horizontal")
+      var cardDiv = document.createElement("div")
+      cardDiv.setAttribute("class", "card-image")
       var poster = document.createElement("img")
+      var stackedDiv = document.createElement("div")
+      stackedDiv.setAttribute("class", "card-stacked")
+      var title = document.createElement("h3")
+      var hr = document.createElement("hr")
+      var director = document.createElement("p")
       var description = document.createElement("p")
       var genres = document.createElement("p")
+      var rating = document.createElement("p")
 
-      card.setAttribute("class", "column is-full")
-      title.textContent = data.Title
+      title.textContent = data.Title + " (" + data.Year + ")"
+      director.textContent = "Director: " + data.Director
       poster.setAttribute("src", data.Poster)
-      description.textContent = data.Plot
-      genres.textContent = data.Genre
+      description.textContent = "Overview: " + data.Plot
+      genres.textContent = "Genres: " + data.Genre
+      rating.textContent = "Rating: " + data.Rated
       
       console.log(title)
         console.log(poster)
         console.log(description)
         console.log(genres);
 
-        card.append(poster, title, description, genres)
+        title.append(hr)
+        stackedDiv.append(title, director, genres, rating, description)
+        cardDiv.append(poster)
+        div2.append(cardDiv)
+        div2.append(stackedDiv)
+        card.append(div2)
         movieInfo.appendChild(card)
     } ) }
